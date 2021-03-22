85d1eaed16aa51cf61d3a5802e8aec78a74d7e99
ThomsonCoding <thomsoncoding@gmail.com>
2021-03-22 21:04:28 +0000

    Practice makes perfect

--- Programming Snippets/CodeWarSnippets.js
+++ Programming Snippets/CodeWarSnippets.js
@@ -293,3 +293,63 @@ numberToString(999); // returns '999';`
 function numberToString(num) {
   return num.toString();
 }
+
+/* Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+
+
+*/
+
+function isPrime(num) {
+  //Inputs num variable.
+  for (
+    var i = 2;
+    i < num;
+    i++ //Iterates through the potential numbers it could be divisible by. i = 2, because you do not want to divide by 0 & 1.
+  )
+    if (num % i === 0) return false; //If the number does divide without a remainer, than it is not a prime number.
+  return num > 1; // Ensure num > 1.
+}
+
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item) {
+  var decider = arr.includes(item); // decider will equal arr.includes item. (If item is included in arr, sets decider to true, if not sets it to false.)
+  return decider; // Returns the answer
+}
+
+/* It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits is 13.
+
+Digits can be a number or a string, and you should control it is no undefined and return an empty string.
+
+To give you a little more excitement, the program will not only write the result of the sum, but also write all the sums used: 3 + 4 + 3 + 3 = 13.
+ */
+
+// Attempt 1, managed to solve the sum. although this was very long winded.
+
+function sum(digits) {
+  var splitArray = digits.toString(digits.length).split("");
+  var numArray = splitArray.map((i) => Number(i));
+  var sum = numArray.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  return sum;
+}
+
+// Manage to reduce the code, making it more effective. Although this has the answer, but not the working out.
+function sum(digits) {
+  var splitArray = digits
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  return splitArray;
+}
