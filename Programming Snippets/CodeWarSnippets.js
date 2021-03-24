bdce5e2fad57c802b7351f5668ccbc9f5469fef7
ThomsonCoding <thomsoncoding@gmail.com>
2021-03-24 07:16:59 +0000

    Practice

--- Programming Snippets/CodeWarSnippets.js
+++ Programming Snippets/CodeWarSnippets.js
@@ -353,3 +353,81 @@ function sum(digits) {
     }, 0);
   return splitArray;
 }
+
+// Solution from other developer.
+
+function sum(d) {
+  return d === undefined
+    ? ""
+    : (d + "").split("").join(" + ") +
+        " = " +
+        (d + "").split("").reduce((a, b) => a + +b, 0);
+}
+/* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes:
+
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+
+Solution:
+*/
+
+function highAndLow(numbers) {
+  var numberMethod = numbers.split(" ").sort((a, b) => a - b); //Split string into seperate numbers, arrange them into order.
+  return numberMethod[numberMethod.length - 1] + " " + numberMethod[0]; //Return the last and the very first numbers within the string.
+}
+
+//Alternative solution:
+
+function highAndLow(numbers) {
+  numbers = numbers.split(" ").map(Number);
+  return Math.max.apply(0, numbers) + " " + Math.min.apply(0, numbers);
+}
+
+//Alternative solution 2:
+
+function highAndLow(numbers) {
+  numbers = numbers.split(" ");
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
+/* Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+Solution
+*/
+
+function descendingOrder(n) {
+  var digits = n.toString().split("");
+  var number = digits
+    .map(Number)
+    .sort((a, b) => b - a)
+    .toString()
+    .split(",")
+    .join("");
+  return parseInt(number, 10);
+}
+
+// Alternative solution
+
+function descendingOrder(n) {
+  return parseInt(String(n).split("").sort().reverse().join(""));
+}
+
+// Alternative solution 2
+function descendingOrder(n) {
+  return parseInt(n.toString().split("").sort().reverse().join(""), 10);
+}
