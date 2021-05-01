daca18f3bd786fa02740f5707881c0c51fb6872b
ThomsonCoding <thomsoncoding@gmail.com>
2021-05-01 10:03:44 +0100

    Responsive updates

--- Projects/Salisbury/JS/Salisburys.js
+++ Projects/Salisbury/JS/Salisburys.js
@@ -6,3 +6,16 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+$("#BookButton").click(function () {
+  $("#bookNow").toggle();
+});
+
+$(document).mouseup(function (e) {
+  var container = $("#bookNow");
+
+  // If the target of the click isn't the container
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.hide();
+  }
+});
