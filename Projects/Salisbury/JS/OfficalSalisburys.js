eae2d53d0022557e6cdc363145003172b99b7288
ThomsonCoding <thomsoncoding@gmail.com>
2021-05-04 10:45:06 +0100

    Updates

--- Projects/Salisbury/JS/OfficalSalisburys.js
+++ Projects/Salisbury/JS/OfficalSalisburys.js
@@ -2,7 +2,30 @@ function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
+    x.className += " w3-animate-right";
   } else {
     x.className = "topnav";
   }
 }
+
+function myFunctionIconChange() {
+  var x = document.getElementById("NavBarMainIcon");
+  if (x.className === "fa fa-bars") {
+    x.className = "fa fa-chevron-right";
+    $(".MainNav").show();
+  } else {
+    x.className = "fa fa-bars";
+    $(".MainNav").hide();
+    $(".BookingNav").hide();
+  }
+}
+
+$(".standOutText").click(function () {
+  $(".MainNav").hide();
+  $(".BookingNav").show();
+});
+
+$(".ReturnButton").click(function () {
+  $(".MainNav").show();
+  $(".BookingNav").hide();
+});
