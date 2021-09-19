5865132197530ddec57fc3b22af6833a134b0c09
ThomsonCoding <thomsoncoding@gmail.com>
2021-09-19 16:16:55 +0100

    Latest updates

--- Fisheye/JS/orderConfirmation.js
+++ Fisheye/JS/orderConfirmation.js
@@ -1,8 +1,22 @@
+// Able to use this to show the selected div, simply add onclick="showDiv('DivsID')" to the HTML property.  
+
+function showDiv(ID) {
+    document.getElementById(ID).style.display = "block";
+ }
+
+// Able to use this to hide the selected div, simply add onclick="showDiv('DivsID')" to the HTML property.  
+
+ function hideDiv(ID) {
+    document.getElementById(ID).style.display = "none";
+ }
+
+ 
+
 // Using jQuery to provide functionality to both the "Subscribe" and "Close" button. Toggle will act like a switch between displaying none and displaying show.
 
-$(function(){
-    $('.ModalButton').on('click',function(){
-        $('.Modal').toggle();
-        $('.ModalBackground').toggle();
-    });
-});
+// $(function(){
+//     $('.ModalButton').on('click',function(){
+//         $('.Modal').toggle();
+//         $('.ModalBackground').toggle();
+//     });
+// });
